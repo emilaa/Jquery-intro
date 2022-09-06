39ca28bf95520b38cf70afdbc649d685f336cef9
emilaa <emilaa@code.edu.az>
2022-09-06 23:03:02 +0400

    Homework

---
+++ assets/js/jquery.js
@@ -0,0 +1,40 @@
+// $(document).ready(function () {});
+
+$(function () {
+  // let elem = document.querySelector("h4");
+  let elem = $("#products h4");
+
+  //   for (const item of elem) {
+  //     console.log(item);
+  //   }
+
+  $("#products button")
+    .first()
+    .click(function () {
+      // console.log($(this).html(`<span class="text-danger">Bye bye</span>`));
+      $("#products .box").hide(2000);
+      $("h4").css({
+        "font-size": "50px",
+        color: "yellow",
+      });
+    });
+
+  $("#products button")
+    .first()
+    .next()
+    .click(function () {
+      // console.log($(this).html(`<span class="text-danger">Bye bye</span>`));
+      $("#products .box").show(2000, function () {
+        $("#products .box").css("background-color", "blue");
+      });
+    });
+
+  $("#products button")
+    .last()
+    .click(function () {
+      // console.log($(this).html(`<span class="text-danger">Bye bye</span>`));
+      $("#products .box").toggle(2000, function () {
+        $("#products .box").css("background-color", "blue");
+      });
+    });
+});
